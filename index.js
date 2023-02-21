b03566016e9e6a270ea26394b73dd101e9dcf570
Connor Kormos <connorkormos97@gmail.com>
2023-02-21 11:48:57 -0800

    complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
 
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 8;
+let num4 = 2;
+
+let mod = num3/num4;
+
+let max = Math.max(5, 10, 15, 20);
+
+/*function multiply(num1, num2) {
+    let multiply = num1 * num2;
+    return multiply(31, 2);
+};
+*/
 // Write your code here
